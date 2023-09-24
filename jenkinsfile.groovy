5b1990fccfa62ffab4fb12416b26b8aeb514d1a3
sharu1301 <reachtosharuk@gmail.com>
2023-09-24 15:02:00 +0530

    ttt

---
+++ jenkinsfile.groovy
@@ -0,0 +1,17 @@
+pipeline{
+
+    agent any
+
+    stages{
+
+        stage('Cleanup workspace'){
+
+            steps{
+                script{
+
+                    cleanWs
+                }
+            }
+        }
+    }
+}
